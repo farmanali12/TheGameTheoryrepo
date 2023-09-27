83ac9b05df391e4f90300a02b48d7a7b68e8b78d
Farman <farmanbehlim1@gmail.com>
2023-09-27 12:25:45 +0530

    add crud functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import BeerCard from "./component/BeerCard"
+import React, { useEffect, useState } from 'react';
+import './App.css'; // Import your CSS file
 
-const App = () => {
-  const [beers, setBeers] = useState([]);
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({ name: '', email: '' });
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch data from the API
-    axios.get('https://api.punkapi.com/v2/beers')
-      .then((response) => {
-        setBeers(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    fetchData();
   }, []);
 
-  const filteredBeers = beers.filter((beer) =>
-    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleCreateUser = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUsers([...users, data]); // Add the new user to the list
+        setNewUser({ name: '', email: '' }); // Clear the input fields
+      } else {
+        console.error('Failed to create user');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleDeleteUser = async (userId) => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        const updatedUsers = users.filter((user) => user.id !== userId);
+        setUsers(updatedUsers);
+      } else {
+        console.error('Failed to delete user');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  // Filter the user list based on the search term
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.email.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
-    <div className="App">
-      <h1>Beer Catalog</h1>
+    <div>
+      <h1>User List</h1>
+
+      {/* Search bar */}
       <input
         type="text"
-        placeholder="Search for beers"
+        placeholder="Search users"
+        value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <div className="beercontainer">
-        {filteredBeers.slice(0, 6).map((beer) => (
-          <BeerCard key={beer.id} beer={beer} />
+
+      <div className="user-cards"> {/* Add a class for the user cards */}
+        {filteredUsers.map((user) => (
+          <div key={user.id} className="user-card"> {/* Add a class for each user card */}
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+            <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+          </div>
         ))}
+      </div>
+
+      <h2>Create User</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newUser.name}
+          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={newUser.email}
+          onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+        />
+        <button onClick={handleCreateUser}>Create</button>
       </div>
     </div>
   );
-};
+}
 
 export default App;
